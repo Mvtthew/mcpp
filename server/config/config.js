483387acf8cc24ef0fe8123bc7c5742b833380a5
Mateusz Ożóg <mateusz2345678@gmail.com>
2020-03-16 18:11:06 +0100

    :lock: Fiexed config in repo

--- server/config/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	mongoURI: 'mongodb://127.0.0.1:27017/mcpp',
-	jwtSecret: 'dkasjhdkjashdjk23bdk2jdfnkj23bfvgi34hv34ui9ovh4980hjejnv89ervutn4785o7vn348957gh9834y9vt7834b6v9g874895738g9v'
-}
