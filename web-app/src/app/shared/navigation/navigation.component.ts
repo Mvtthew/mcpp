df993d43264f169c4fd4c6bee830f8cb2d6c5be2
Mateusz Ożóg <mateusz2345678@gmail.com>
2020-03-26 14:00:25 +0100

    :lipstick: Navigation + menu

--- web-app/src/app/shared/navigation/navigation.component.ts
+++ web-app/src/app/shared/navigation/navigation.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-navigation',
-  templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.scss']
+	selector: 'app-navigation',
+	templateUrl: './navigation.component.html',
+	styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+	menuVisible: boolean = false;
 
-  ngOnInit(): void {
-  }
+	constructor(private router: Router) { }
+
+	ngOnInit(): void {
+		this.router.events.subscribe(() => {
+			this.menuVisible = false;
+		});
+	}
+
+	toggleMenu(): void {
+		this.menuVisible = !this.menuVisible;
+	}
 
 }
